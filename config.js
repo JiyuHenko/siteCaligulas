17814bc5bef17b466148c84e09b9838626499893
JiyuHenko <87832816+JiyuHenko@users.noreply.github.com>
2026-08-31 07:50:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CALIGULAS_CONFIG = Object.freeze({
-  appsScriptUrl: "",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwq4zJ5k8OaIgj9IFTHsMtr-D683RPaE0iW6_ayKNynHEIorrrxgh-_oEPei79VegBB/exec",
   instagram: "https://www.instagram.com/caligulaspokerlive/",
   whatsappNumber: "5535988369317",
   whatsappMessage: "Olá! Vim pelo site do Caligulas Poker Live.",
